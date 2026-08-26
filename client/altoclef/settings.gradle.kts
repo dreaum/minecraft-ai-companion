c654e568824de4c781dd95fda69f9513fca078a5
dreaum <qddwqdw@163.com>
2026-08-27 05:44:20 +0800

    build: narrow client project to 1.20.1

--- client/altoclef/settings.gradle.kts
+++ client/altoclef/settings.gradle.kts
@@ -20,24 +20,11 @@ pluginManagement {
 rootProject.name = "altoclef"
 rootProject.buildFileName = "root.gradle.kts"
 
-listOf(
-    "1.21.1",
-    "1.21",
-    "1.20.6",
-    "1.20.5",
-    "1.20.4",
-    "1.20.2",
-    "1.20.1",
-    "1.19.4",
-    "1.18.2",
-    "1.18",
-    "1.17.1",
-    "1.16.5"
-).forEach { version ->
-    include(":$version")
-    project(":$version").apply {
-        projectDir = file("versions/$version")
-        buildFileName = "../../build.gradle"
-        name = version
-    }
+val targetVersion = "1.20.1"
+
+include(":$targetVersion")
+project(":$targetVersion").apply {
+    projectDir = file("versions/$targetVersion")
+    buildFileName = "../../build.gradle"
+    name = targetVersion
 }
